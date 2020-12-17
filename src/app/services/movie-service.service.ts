68b4a2a3903ec981f82ca7f9d9bab5a9c890b484
Glen Wu <glenwu1998@gmail.com>
2020-12-16 18:59:14 -0800

    try with your api_key to make it work

--- src/app/services/movie-service.service.ts
+++ src/app/services/movie-service.service.ts
@@ -18,19 +18,18 @@ import { Movie } from '../models/movie';
 export class MovieServiceService {
   // basicUrl: String = "https://api.themoviedb.org/3/";
   // baseUrl: string = "http://localhost:4200/";
-  // searchUrl: string = 'https://api.themoviedb.org/3/search/movie?api_key=0a1ed8f16ba545aa32cf042f2f90ce59&language=en-US&query=spider&page=1&include_adult=false';
   constructor(private http: HttpClient) { }
 
   // Call search Url to get a movie
   getMovie(movieName:string) {
     movieName = encodeURIComponent(movieName);
-    var searchUrl:string = `https://api.themoviedb.org/3/search/movie?api_key=0a1ed8f16ba545aa32cf042f2f90ce59&language=en-US&query=${movieName}&page=1&include_adult=false`;
+    var searchUrl:string = `https://api.themoviedb.org/3/search/movie?api_key={put_api_key_here}&language=en-US&query=${movieName}&page=1&include_adult=false`;
     return this.http.get<Movie>(searchUrl);
   }
 
 
   getLatestMovie() {
-    var latestMovieURL:string = "https://api.themoviedb.org/3/movie/latest?api_key=0a1ed8f16ba545aa32cf042f2f90ce59&language=en-US";
+    var latestMovieURL:string = "https://api.themoviedb.org/3/movie/latest?api_key={put_api_key_here}&language=en-US";
     return this.http.get(latestMovieURL);
   }
 
